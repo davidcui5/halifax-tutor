c8194d13d1583772c4497c88b596d6c4a8c402f8
Zongming Liu <zongmingxliu@gmail.com>
2018-05-31 15:16:32 -0300

    initialize the starting point of the application

---
+++ src/main/java/group12/App.java
@@ -0,0 +1,15 @@
+package group12;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+
+}
