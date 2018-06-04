6ad496da6b5b2deaadd5380b842a88f56b5d606a
Zongming Liu <zongmingxliu@gmail.com>
2018-06-04 15:44:54 -0300

    Build a WAR file

--- src/main/java/group12/App.java
+++ src/main/java/group12/App.java
@@ -2,14 +2,22 @@ package group12;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @EnableAutoConfiguration
-public class App {
+public class App extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);
     }
+
 
 }
