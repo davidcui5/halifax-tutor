bf8fb56c6afe7b8698d96b07f1bda8ba661f35f2
Zongming Liu <zongmingxliu@gmail.com>
2018-06-04 22:23:52 -0300

    use war file

--- src/main/java/group12/App.java
+++ src/main/java/group12/App.java
@@ -18,6 +18,4 @@ public class App extends SpringBootServletInitializer {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);
     }
-
-
 }
