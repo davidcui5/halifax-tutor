1bb650769a3ae8ad606efb3304edb20a93559f3e
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-06-10 19:51:36 -0300

    return the app.java to old

--- src/main/java/group12/App.java
+++ src/main/java/group12/App.java
@@ -4,8 +4,6 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
@@ -18,9 +16,6 @@ public class App extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) throws Exception {
-//        SpringApplication.run(App.class, args);
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        DBDAO dbda = context.getBean("studentDAOJDBCTemplate", DBDAO.class);
-        System.out.println(dbda.authorizeStudent("zaher88abd@gmail.com", "zaher"));
+        SpringApplication.run(App.class, args);
     }
 }
