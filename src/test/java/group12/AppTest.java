321b2c78413164644f46a7be4c0cfe6529df8264
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-06-10 19:49:53 -0300

    Test DataBase Connection

--- src/test/java/group12/AppTest.java
+++ src/test/java/group12/AppTest.java
@@ -1,5 +1,16 @@
 package group12;
 
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static junit.framework.TestCase.assertTrue;
+
 public class AppTest {
 
+    @Test
+    public void testDBConnection() {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        DBDAO dbda = context.getBean("studentDAOJDBCTemplate", DBDAO.class);
+        assertTrue(dbda.authorizeStudent("zaher88abd@gmail.com", "zaher"));
+    }
 }
