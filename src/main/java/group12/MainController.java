bfa5215d2b5476dac767ad3e1f6e7029c0ad383e
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-06-18 01:05:15 -0300

    Send email to the student email

--- src/main/java/group12/MainController.java
+++ src/main/java/group12/MainController.java
@@ -8,52 +8,57 @@ import org.springframework.web.bind.annotation.*;
 public class MainController {
 
     @Autowired
-    private DBDAO db=new DBDAO();
+    private DBDAO db = new DBDAO();
+
+    @Autowired
+    private MailMail m = new MailMail();
 
 
-    @GetMapping(path="/user")
-    public @ResponseBody String login(@RequestBody User user){
-        if(db.authorizeStudent(user.getEmail(),user.getPassword()))
+    @GetMapping(path = "/user")
+    public @ResponseBody
+    String login(@RequestBody User user) {
+        if (db.authorizeStudent(user.getEmail(), user.getPassword()))
             return "student";
-        if(db.authorizeTutor(user.getEmail(),user.getPassword()))
+        if (db.authorizeTutor(user.getEmail(), user.getPassword()))
             return "tutor";
         return "not found";
     }
 
-    @PostMapping(path="/student")
-    public @ResponseBody String studentRegister(@RequestBody Student student){
+    @PostMapping(path = "/student")
+    public @ResponseBody
+    String studentRegister(@RequestBody Student student) {
         String response = "";
 
-        if(db.isEmailNew(student.getEmail()))
+        if (db.isEmailNew(student.getEmail()))
             response += "Email already registered\n";
-        if(db.isPhoneNumberNew(student.getPhoneNumber()))
+        if (db.isPhoneNumberNew(student.getPhoneNumber()))
             response += "Phone already registered\n";
 
-        if(response.equals("")){
+        if (response.equals("")) {
             db.regStudent(student);
+            m.sendMail("zaher88abd@gmail.com", student.getEmail(), "Activation", "Plase activate the email");
             return "registration success";
-        }
-        else
+        } else
             return response;
     }
 
-    @PostMapping(path="/tutor")
-    public @ResponseBody String teacherRegister(@RequestBody Tutor tutor){
+    @PostMapping(path = "/tutor")
+    public @ResponseBody
+    String teacherRegister(@RequestBody Tutor tutor) {
 
         String response = "";
 
-        if(!db.isEmailNew(tutor.getEmail()))
+        if (!db.isEmailNew(tutor.getEmail()))
             response += "Email already registered\n";
-        if(!db.isPhoneNumberNew(tutor.getPhoneNumber()))
+        if (!db.isPhoneNumberNew(tutor.getPhoneNumber()))
             response += "Phone already registered\n";
-        if(!db.isCreditCardNew(tutor.getCreditCardNumber()))
+        if (!db.isCreditCardNew(tutor.getCreditCardNumber()))
             response += "Card already registered";
 
-        if(response.equals("")){
+        if (response.equals("")) {
             db.regTutor(tutor);
             return "registration success";
-        }
-        else
+        } else
             return response;
     }
 }
