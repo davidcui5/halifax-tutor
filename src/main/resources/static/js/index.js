79646aa194e2300f2606089827210bbbb4e17ca5
Rahul <rahulvala1502@gmail.com>
2018-06-27 03:15:14 -0300

    Student Register and Login Validation

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,7 +1,42 @@
 'use strict';
 
 $(document).ready(function () {
-    $("form").submit(function ( event ) {
-        alert("hello, world")
-    });
+    $("form").submit(
+        function validateForm(){
+            var email = document.forms["user_login"]["user_email"].value;
+            var password = document.forms["user_login"]["user_password"].value;
+            // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+            var validateEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            var validateName = /^[A-Za-z]+$/;
+
+            // validate email
+            if(!validateEmail.test(String(email).toLowerCase())){
+                alert("Invalid Email Address format");
+                $('#user_email').after('<p>Invalid Email Address format</p>')
+                email.focus();
+                return false;
+            }
+
+            // validate passwords
+            if (validateName.test(password)){
+                alert("Passwords must contain atleast one number");
+                $('#user_password').after('<p>Passwords must contain atleast one number</p>')
+                password.focus();
+                return false;
+            }
+
+            if (password.length < 8){
+                alert("Passwords must contain atleast 8 characters");
+                $('#user_password').after('<p>Passwords must contain atleast 8 characters</p>')
+                password.focus();
+                return false;
+            }
+
+            if (password.toLowerCase() == password){
+                alert("Passwords must contain atleast one Capital letter");
+                password.focus();
+                return false;
+            }
+        }
+    );
 });
--- src/main/resources/static/js/student-register.js
+++ src/main/resources/static/js/student-register.js
@@ -37,4 +37,76 @@ $(document).ready(function () {
         });
 
     });
+
+    $("form").submit(function validateForm(){
+        var email = document.forms["user_register"]["user_email"].value;
+        var password = document.forms["user_register"]["user_password"].value;
+        var confirmPassword = document.forms["user_register"]["user_repeat_password"].value;
+        var phoneNumber = document.forms["user_register"]["user_phone_number"].value;
+        var firstName = document.forms["user_register"]["user_first_name"].value;
+        var lastName = document.forms["user_register"]["user_last_name"].value;
+
+        // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+        var validateEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        // https://www.w3resource.com/javascript/form/phone-no-validation.php
+        var validatePhone = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+        var validateName = /^[A-Za-z]+$/;
+
+        // validate email
+        if(!validateEmail.test(String(email).toLowerCase())){
+            alert("Invalid Email Address format");
+            email.focus();
+            return false;
+        }
+
+        // validate passwords
+        if (validateName.test(password)){
+            alert("Passwords must contain atleast one number");
+            password.focus();
+            return false;
+        }
+
+        if (password.length < 8){
+            alert("Passwords must contain atleast 8 characters");
+            password.focus();
+            return false;
+        }
+
+        if (password.toLowerCase() == password){
+            alert("Passwords must contain atleast one Capital letter");
+            password.focus();
+            return false;
+        }
+
+        if (password !==  confirmPassword){
+            alert("Passwords do not match.");
+            password.focus();
+            return false;
+        }
+
+        // validate phone number
+        if(!validatePhone.test(phoneNumber) || phoneNumber.length != 10){
+            alert("Invalid Phone Number format");
+            phoneNumber.focus();
+            return false;
+        }
+
+        // validate first name
+        if( !validateName.test(firstName)){
+            alert("First Name can't contain a number");
+            firstName.focus();
+            return false;
+        }
+
+        // validate last name
+        if( !validateName.test(lastName)){
+            console.log(lastName)
+            alert("Last Name can't contain a number");
+            lastName.focus();
+            return false;
+        }
+
+    });
+
+
 });
