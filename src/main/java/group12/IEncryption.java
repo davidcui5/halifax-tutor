807c9c908cb6284e634478857d3762e21fb51850
davidcui5 <yq506499@dal.ca>
2018-06-28 01:22:35 -0300

    0.1

---
+++ src/main/java/group12/IEncryption.java
@@ -0,0 +1,7 @@
+package group12;
+
+public interface IEncryption {
+
+    String encrypt(String input);
+
+}
---
+++ src/main/java/group12/SimpleEncryption.java
@@ -0,0 +1,9 @@
+package group12;
+
+public class SimpleEncryption implements IEncryption {
+
+    public String encrypt (String input){
+        return input;
+    }
+
+}
