9936ec660d05d78da28a6a3cee48201c2430fc82
zaher88abd <zaher88abd@gmail.com>
2018-07-07 20:41:12 -0300

    create method to return objects

--- src/test/java/group12/DBDAOTest/MockData.java
+++ src/test/java/group12/DBDAOTest/MockData.java
@@ -1,4 +1,27 @@
 package group12.DBDAOTest;
 
+import group12.Registration.Student;
+import group12.Registration.Tutor;
+
 public class MockData {
+    private static Student getStudentObject() {
+        Student student = new Student();
+        student.setEmail("testemailStu@gmail.com");
+        student.setFirstName("testName");
+        student.setLastName("testLName");
+        student.setSchool("testSchool");
+        student.setPhoneNumber("9021234567S");
+        student.setPassword("pass123456S");
+        return student;
+    }
+
+    private static Tutor getTutorObject() {
+        Tutor tutor = new Tutor();
+        tutor.setEmail("testemailTut@gmail.com");
+        tutor.setFirstName("testName");
+        tutor.setLastName("testLName");
+        tutor.setPhoneNumber("9021234567T");
+        tutor.setPassword("pass123456T");
+        return tutor;
+    }
 }
