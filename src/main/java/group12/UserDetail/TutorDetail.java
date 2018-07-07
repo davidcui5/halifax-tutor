4bd544f78e598cceba7e72f64018f33001d5ff69
youranzhang <yr994423@dal.ca>
2018-07-07 12:45:37 -0300

    TutorSetting Frontend S1

---
+++ src/main/java/group12/UserDetail/TutorDetail.java
@@ -0,0 +1,10 @@
+package group12.UserDetail;
+
+public class TutorDetail {
+    public String email;
+    public String photo;
+    private String phoneNumber;
+    private String firstName;
+    private String lastName;
+
+}
