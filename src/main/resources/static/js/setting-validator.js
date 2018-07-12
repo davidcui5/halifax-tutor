3bea65378e8aa55a3a84c5576d18fc13836c3f95
youranzhang <yr994423@dal.ca>
2018-07-12 14:58:43 -0300

    validate pwd before change

--- src/main/resources/static/js/setting-validator.js
+++ src/main/resources/static/js/setting-validator.js
@@ -1,9 +1,51 @@
 'use strict';
+function validatePwd(){
+    var inputs = document.forms["Cpassword"].getElementsByTagName("input");
+    var flag = true;
+    for (var i=0; i<inputs.length; i++) {
+        var element = document.getElementById(inputs[i].id);
+        var elementValue = document.getElementById(inputs[i].id).value;
+        alert("Passwords must contain atleast one number");
+
+        // validate password
+        if(inputs[i].id == "pwd"){
+
+            if (validateName.test(elementValue)){
+                alert("Passwords must contain atleast one number");
+                element.focus();
+                flag = false;
+            }
+
+            if (elementValue.length < 8){
+                alert("Passwords must contain atleast 8 characters");
+                element.focus();
+                flag = false;
+            }
+
+            if (elementValue.toLowerCase() == elementValue){
+                alert("Passwords must contain atleast one Capital letter");
+                element.focus();
+                flag = false;
+            }
+        }
+
+        // validate confirm password
+        if(inputs[i].id == "Rpwd") {
+            var passwordValue = document.getElementById("password").value;
+            if (elementValue !==  passwordValue){
+                alert("Passwords do not match.");
+                element.focus();
+                return false;
+            }
+        }
+    }
+}
 function validateEmail(){
+     var inputs = document.forms["Cemail"].getElementsByTagName("input");
     var validateEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     var flag = true;
     for (var i=0; i<inputs.length; i++) {
-        var element = document.getElementById(inputs[i].id)
+        var element = document.getElementById(inputs[i].id);
         var elementValue = document.getElementById(inputs[i].id).value;
 
         // validate email
