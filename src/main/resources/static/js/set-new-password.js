89dc513a99333e08673aacb413f8c67b1f06b41b
Rahul <rahulvala1502@gmail.com>
2018-07-13 15:28:29 -0300

    Initial Code (Error)

--- src/main/resources/static/js/set-new-password.js
+++ src/main/resources/static/js/set-new-password.js
@@ -3,24 +3,27 @@
 $(document).ready(function () {
     $("form").submit(function (event) {
         event.preventDefault();
-        let email = $("#email").val();
+        let new_password = $("#password").val();
+        let email = $("#email").text();
+        let usertype = $("#usertype").text();
 
-        var studentData = {
-            "email": email
+        var userData = {
+            "email": email,
+            "password": new_password
         };
 
-
-        if (document.getElementById('student').checked){
+        if (usertype == "student"){
             $.ajax({
 
-                url: "http://localhost:8080/studentforgotpassword",
-                data: JSON.stringify(studentData),
+                url: "http://localhost:8080/setnewpasswordstudent",
+                data: JSON.stringify(userData),
                 contentType: "application/json",
                 type: "POST",
                 dataType: "text"
             }).done(function (text) {
-                if (text === "verification email sent") {
-                    alert("Verification email has been sent!");
+                if (text === "password update success") {
+                    alert("Password Updated !");
+                    window.location.replace("../index.html");
                 } else {
                     alert("Something went wrong: " + text);
                 }
@@ -28,16 +31,17 @@ $(document).ready(function () {
 
             });
         }
-        else{
+        else {
             $.ajax({
-                url: "http://localhost:8080/tutorforgotpassword",
-                data: JSON.stringify(studentData),
+                url: "http://localhost:8080/setnewpasswordtutor",
+                data: JSON.stringify(userData),
                 contentType: "application/json",
                 type: "POST",
                 dataType: "text"
             }).done(function (text) {
-                if (text === "verification email sent") {
-                    alert("Verification email has been sent!");
+                if (text === "password update success") {
+                    alert("Password Updated !");
+                    window.location.replace("../index.html");
                 } else {
                     alert("Something went wrong: " + text);
                 }
@@ -45,6 +49,7 @@ $(document).ready(function () {
 
             });
         }
+
 
 
     });
