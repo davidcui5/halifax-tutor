31e9d488b91a7b959b89e10a5d6a34a3e1411278
davidcui5 <yq506499@dal.ca>
2018-07-13 00:40:36 -0300

    Finished GetStudentSQLOperation

--- src/main/java/group12/data_access/GetStudentSQLOperation.java
+++ src/main/java/group12/data_access/GetStudentSQLOperation.java
@@ -33,9 +33,15 @@ public class GetStudentSQLOperation extends SQLOperationTemplate {
     @Override
     Object extractResultSet(ResultSet rs) throws SQLException {
         Student student = new Student();
+        student.setEmail(rs.getString("Email"));
         student.setPassword(rs.getString("Password"));
+        student.setStudentID(rs.getInt("ID"));
+        student.setFirstName(rs.getString("FirstName"));
+        student.setLastName(rs.getString("LastName"));
         student.setActivated(rs.getBoolean("AccountActivation"));
         student.setBanned(rs.getBoolean("Banned"));
+        student.setPhoneNumber(rs.getString("PhoneNumber"));
+        student.setSchool(rs.getString("School"));
         return student;
     }
 
