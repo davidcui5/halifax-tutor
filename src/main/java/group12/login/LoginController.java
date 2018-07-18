669413815b199e29e17acac397642f4ce8b8768a
davidcui5 <yq506499@dal.ca>
2018-07-17 23:32:58 -0300

    Removed some beans in spring.xml

--- src/main/java/group12/login/LoginController.java
+++ src/main/java/group12/login/LoginController.java
@@ -2,7 +2,6 @@ package group12.login;
 
 import group12.data_access.*;
 import group12.encryption.*;
-import group12.token_auth.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
