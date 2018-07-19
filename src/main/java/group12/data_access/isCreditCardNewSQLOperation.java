243e2d97ecb802bf7d307e44ef7de266e50eed0b
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-07-19 18:58:15 -0300

    Finish the isCardNew

--- src/main/java/group12/data_access/isCreditCardNewSQLOperation.java
+++ src/main/java/group12/data_access/isCreditCardNewSQLOperation.java
@@ -1,4 +1,36 @@
 package group12.data_access;
 
-public class isCreditCardNewSQLOperation {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class isCreditCardNewSQLOperation extends SQLOperationTemplate {
+    public isCreditCardNewSQLOperation(Object... parameters) {
+        super(parameters);
+    }
+
+    @Override
+    String makeSQL() {
+        return "SELECT IsCreditCardNew(?)";
+    }
+
+    @Override
+    PreparedStatement addParameters(PreparedStatement ps) throws SQLException {
+        String cardNumber = (String) getParameters().get(0);
+        ps.setString(1, cardNumber);
+        return ps;
+    }
+
+    @Override
+    Object extractResultSet(ResultSet rs) throws SQLException {
+        int numberOfCards;
+        rs.next();
+        numberOfCards = rs.getInt(1);
+        return numberOfCards;
+    }
+
+    @Override
+    ResultSet execute(PreparedStatement ps) throws SQLException {
+        return ps.executeQuery();
+    }
 }
