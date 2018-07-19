0fa0c5b59255803a4d1a42a91abd2f25bd468274
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-07-19 16:03:47 -0300

    Finish isPhoneNew

--- src/main/java/group12/data_access/IsPhoneNewSQLOperation.java
+++ src/main/java/group12/data_access/IsPhoneNewSQLOperation.java
@@ -1,4 +1,33 @@
 package group12.data_access;
 
-public class IsPhoneNewSQLOperation {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class IsPhoneNewSQLOperation extends SQLOperationTemplate {
+
+    @Override
+    String makeSQL() {
+        return "SELECT IsPhoneNew(?)";
+    }
+
+    @Override
+    PreparedStatement addParameters(PreparedStatement ps) throws SQLException {
+        String phone = (String) getParameters().get(0);
+        ps.setString(1, phone);
+        return ps;
+    }
+
+    @Override
+    Object extractResultSet(ResultSet rs) throws SQLException {
+        int numberOfPhones;
+        rs.next();
+        numberOfPhones = rs.getInt(1);
+        return numberOfPhones;
+    }
+
+    @Override
+    ResultSet execute(PreparedStatement ps) throws SQLException {
+        return ps.executeQuery();
+    }
 }
