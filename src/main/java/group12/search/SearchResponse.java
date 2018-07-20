3ea13f53f8d93074957114d0e96d44ea486fe62f
Zongming Liu <zongmingxliu@gmail.com>
2018-07-20 19:30:06 -0300

    add field to SearchResponse

--- src/main/java/group12/search/SearchResponse.java
+++ src/main/java/group12/search/SearchResponse.java
@@ -1,17 +1,23 @@
 package group12.search;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchResponse {
     private boolean success;
     private int numOfResults;
+    private List<TutorPublicInfo> results;
 
     public SearchResponse() {
         this.success = false;
         this.numOfResults = 0;
+        this.results = new ArrayList<>();
     }
 
-    public SearchResponse(boolean success, int numOfResults) {
+    public SearchResponse(boolean success, int numOfResults, List<TutorPublicInfo> results) {
         this.success = success;
         this.numOfResults = numOfResults;
+        this.results = results;
     }
 
 
@@ -23,11 +29,19 @@ public class SearchResponse {
         this.numOfResults = numOfResults;
     }
 
+    public void setResults(List<TutorPublicInfo> results) {
+        this.results = results;
+    }
+
     public int getNumOfResults() {
         return numOfResults;
     }
 
     public boolean getSuccess() {
         return success;
+    }
+
+    public List<TutorPublicInfo> getResults() {
+        return results;
     }
 }
