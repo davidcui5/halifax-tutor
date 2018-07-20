f92ec8f9290e5383665c2d694fba9a73e5cd8c47
Zongming Liu <zongmingxliu@gmail.com>
2018-07-20 19:27:18 -0300

    add implementation for TutorPublicInfoDao

---
+++ src/main/java/group12/search/TutorPublicInfoDaoImpl.java
@@ -0,0 +1,10 @@
+package group12.search;
+
+import java.util.List;
+
+public class TutorPublicInfoDaoImpl implements TutorPublicInfoDAO {
+    @Override
+    public List<TutorPublicInfo> getTutorPublicInfo(String school, String courseName) {
+        return null;
+    }
+}
