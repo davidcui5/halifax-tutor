38ce3dbeed1b1f885a6c849094e810db7c915e44
davidcui5 <yq506499@dal.ca>
2018-07-19 23:07:53 -0300

    JS for admin setting

--- src/main/java/group12/admin_setting/AdminController.java
+++ src/main/java/group12/admin_setting/AdminController.java
@@ -4,27 +4,45 @@ import group12.token_auth.IAccessToken;
 import group12.token_auth.JWTAccessToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping(path="/admin/setting")
 public class AdminController {
 
-    @PostMapping(path="/admin/setting/access")
+    IAccessToken decoder;
+
+    public AdminController(){
+        decoder = JWTAccessToken.getInstance();
+    }
+
+    @PostMapping(path="/access")
     public String authorize(@RequestBody Map<String,String> body){
-        String token = body.get("token");
-        IAccessToken decoder = JWTAccessToken.getInstance();
-        String email = decoder.decodeToken(token);
+        String email = decoder.decodeToken(body.get("token"));
         /*if(admin table contain this email){
-            return SUCCESS;
+            return AUTHORIZED;
         }
         else{
-            return FAILURE;
+            return UNAUTHORIZED;
+        }*/
+        return "AUTHORIZED";
+    }
+
+    @PostMapping(path="/password")
+    public String changePassword(@RequestBody Map<String,String> body){
+        String email = decoder.decodeToken(body.get("token"));
+        String password = body.get("password");
+        /*if(admin table contain this email){
+            return AUTHORIZED;
+        }
+        else{
+            return UNAUTHORIZED;
         }*/
         return "SUCCESS";
     }
-
 
 
 }
--- src/main/resources/static/js/admin-setting.js
+++ src/main/resources/static/js/admin-setting.js
@@ -11,13 +11,196 @@ $(document).ready(function () {
         type: "POST",
         dataType: "text"
     }).done(function (data) {
-        if (data === "FAILURE") {
+        if (data === "UNAUTHORIZED") {
             alert("You do not have access to this page.");
             window.location.replace("../index.html");
         }
     }).fail(function (xhr, status, errorThrown) {
-
+        window.location.replace("../index.html"); //maybe error page is better.
     });
 
+    $("#Cpassword").submit(function (event) {
+        event.preventDefault();
+        let password = $("#pwd").val();
+
+        var data = {
+            'token': token,
+            "password": password
+        };
+
+        $.ajax({
+            url: location.origin + "/admin/setting/password",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            type: "POST",
+            dataType: "text"
+        }).done(function (data) {
+
+        }).fail(function (xhr, status, errorThrown) {
+            //empty
+        });
+    });
+
+
+    $("#Cprice").submit(function (event) {
+        event.preventDefault();
+        let priceOne = $("#priceOne").val();
+        let priceTwo = $("#priceTwo").val();
+        let priceThree = $("#priceThree").val();
+        let priceFour = $("#priceFour").val();
+
+        var data = {
+            'token': token,
+            'priceOne': priceOne,
+            'priceTwo': priceTwo,
+            'priceThree': priceThree,
+            'priceFour': priceFour
+        };
+
+        $.ajax({
+            url: location.origin + "/",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            type: "POST",
+            dataType: "text"
+        }).done(function (data) {
+
+        }).fail(function (xhr, status, errorThrown) {
+            //empty
+        });
+    });
+
+    $("#banStudent").submit(function (event) {
+        event.preventDefault();
+        let studentID = $("#SbanID").val();
+
+        var data = {
+            'token': token,
+            'studentID': studentID
+        };
+
+        $.ajax({
+            url: location.origin + "/",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            type: "POST",
+            dataType: "text"
+        }).done(function (data) {
+
+        }).fail(function (xhr, status, errorThrown) {
+            //empty
+        });
+    });
+
+    $("#unbanStudent").submit(function (event) {
+        event.preventDefault();
+        let studentID = $("#SunbanID").val();
+
+        var data = {
+            'token': token,
+            'studentID': studentID
+        };
+
+        $.ajax({
+            url: location.origin + "/",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            type: "POST",
+            dataType: "text"
+        }).done(function (data) {
+
+        }).fail(function (xhr, status, errorThrown) {
+            //empty
+        });
+    });
+
+
+    $("#banTutor").submit(function (event) {
+        event.preventDefault();
+        let tutorID = $("#TbanID").val();
+
+        var data = {
+            'token': token,
+            'tutorID': tutorID
+        };
+
+        $.ajax({
+            url: location.origin + "/",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            type: "POST",
+            dataType: "text"
+        }).done(function (data) {
+
+        }).fail(function (xhr, status, errorThrown) {
+            //empty
+        });
+    });
+
+    $("#unbanTutor").submit(function (event) {
+        event.preventDefault();
+        let tutorID = $("#TunbanID").val();
+
+        var data = {
+            'token': token,
+            'tutorID': tutorID
+        };
+
+        $.ajax({
+            url: location.origin + "/",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            type: "POST",
+            dataType: "text"
+        }).done(function (data) {
+
+        }).fail(function (xhr, status, errorThrown) {
+            //empty
+        });
+    });
+
+    $("#findStudent").submit(function (event) {
+        event.preventDefault();
+        let email = $("#SEmail").val();
+
+        var data = {
+            'token': token,
+            'email': email
+        };
+
+        $.ajax({
+            url: location.origin + "/",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            type: "POST",
+            dataType: "text"
+        }).done(function (data) {
+
+        }).fail(function (xhr, status, errorThrown) {
+            //empty
+        });
+    });
+
+    $("#findTutor").submit(function (event) {
+        event.preventDefault();
+        let email = $("#TEmail").val();
+
+        var data = {
+            'token': token,
+            'email': email
+        };
+
+        $.ajax({
+            url: location.origin + "/",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            type: "POST",
+            dataType: "text"
+        }).done(function (data) {
+
+        }).fail(function (xhr, status, errorThrown) {
+            //empty
+        });
+    });
 
 });
