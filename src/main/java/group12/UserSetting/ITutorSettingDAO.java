a273002066a74e8b85cc59fded70130f2adcf7f9
yrzzzzzz <36334989+yrzzzzzz@users.noreply.github.com>
2018-07-22 00:50:31 -0300

    create tutor setting DAO

--- src/main/java/group12/UserSetting/ITutorSettingDAO.java
+++ src/main/java/group12/UserSetting/ITutorSettingDAO.java
@@ -1,4 +1,14 @@
 package group12.UserSetting;
 
+
 public interface ITutorSettingDAO {
+    int countTutorByEmail(String email);
+    boolean setTutorPassword(String email, String password);
+    boolean setTutorEmail(String email);
+    boolean setTutorPhone(String email, String phone);
+    boolean setTutorCard(String email, String creditCardHolder,String creditCardNum,String creditCardExpiryDate,int securityCode);
+    boolean setEdu(String email, String education);
+    boolean setExperience (String email, String experience);
+    boolean addCourse(String email,String school,int code, String price);
+    
 }
