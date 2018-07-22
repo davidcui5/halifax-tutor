3a11c8cd7bc3cdcf882107e14704aa84fd5fab78
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-07-22 18:22:42 -0300

    Add logout method & password

--- src/main/resources/static/js/student-setting.js
+++ src/main/resources/static/js/student-setting.js
@@ -1,9 +1,53 @@
 'use strict';
 
 $(document).ready(function () {
-    let token = localStorage.getItem("token");
-    if (token === null) {
-        alert("You are not logged in. Please log in to search for tutors.");
-//        window.location.assign("../index.html");
-    }
+    var token = localStorage.getItem("token");
+    var objToken = {'token': token};
+
+    $.ajax({
+        url: location.origin + "/admin/setting/access",
+        data: JSON.stringify(objToken),
+        contentType: "application/json",
+        type: "POST",
+        dataType: "text"
+    }).done(function (data) {
+        if (data === "UNAUTHORIZED") {
+            alert("You do not have access to this page.");
+            window.location.replace("../index.html");
+        }
+    }).fail(function (xhr, status, errorThrown) {
+        //window.location.replace("../index.html");
+    });
+
+    $("#logout").click(function (event) {
+        event.preventDefault();
+        localStorage.removeItem("token");
+    });
+
+    $("#Cpassword").submit(function (event) {
+        event.preventDefault();
+
+        if($("#pwd").val() != $("#Rpwd").val()){
+            alert("The two password fields didn't match.")
+            return;
+        }
+
+        let password = $("#pwd").val();
+        var data = {
+            'token': token,
+            "password": password
+        };
+
+        $.ajax({
+            url: location.origin + "/admin/setting/password",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            type: "POST",
+            dataType: "text"
+        }).done(function (data) {
+            alert(data);
+        }).fail(function (xhr, status, errorThrown) {
+            //empty
+        });
+    });
 });
