1397809ab0ad76cd710581e2d90af2b552172322
Zongming Liu <zongmingxliu@gmail.com>
2018-07-21 22:38:36 -0300

    create class for handling SQL operations about TutorPublicInfo for search

---
+++ src/main/java/group12/data_access/GetTutorPublicInfoSQLOperation.java
@@ -0,0 +1,22 @@
+package group12.data_access;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GetTutorPublicInfoSQLOperation extends SQLOperationTemplate {
+    @Override
+    String makeSQL() {
+        return null;
+    }
+
+    @Override
+    PreparedStatement addParameters(PreparedStatement ps) throws SQLException {
+        return null;
+    }
+
+    @Override
+    Object extractResultSet(ResultSet rs) throws SQLException {
+        return null;
+    }
+}
