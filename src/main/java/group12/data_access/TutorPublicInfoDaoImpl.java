c4a52c1937983392e50ec6fb0ec96bab9c31af20
Zongming Liu <zongmingxliu@gmail.com>
2018-07-21 23:20:47 -0300

    finish the TutorPublicInfoDaoImpl

--- src/main/java/group12/data_access/TutorPublicInfoDaoImpl.java
+++ src/main/java/group12/data_access/TutorPublicInfoDaoImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class TutorPublicInfoDaoImpl implements TutorPublicInfoDAO {
     @Override
     public List<TutorPublicInfo> getTutorPublicInfo(String school, String courseName) {
-        return null;
+        SQLOperationTemplate sqlOperationTemplate = new GetTutorPublicInfoSQLOperation(school, courseName);
+        return ((GetTutorPublicInfoSQLOperation) sqlOperationTemplate).executeMysqlQuery();
     }
 }
