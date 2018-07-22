88519cab4bf6c2425fbde907548afd17df5d1ee5
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-07-22 14:07:13 -0300

    Change script of reading token

--- src/main/resources/static/js/student-setting.js
+++ src/main/resources/static/js/student-setting.js
@@ -1,25 +1,9 @@
 'use strict';
-var Doc;
 
 $(document).ready(function () {
-    var token = localStorage.getItem("token");
-    var objToken = {'token': token};
-
-    if (token != null) {
-        $.get({
-            url: "https://csci5308group12devint.azurewebsites.net/check_account",
-            data: JSON.stringify(objToken),
-            contentType: "application/json",
-            type: "GET",
-            dataType: "text"
-        }).done(function (succeed) {
-            Doc = succeed;
-            if (succeed === false) {
-                // similar behavior as an HTTP redirect
-                window.location.replace("https://csci5308group12devint.azurewebsites.net");
-            }
-        }).fail(function (xhr, status, errorThrown) {
-            window.location.replace("https://csci5308group12devint.azurewebsites.net");
-        });
+    let token = localStorage.getItem("token");
+    if (token === null) {
+        alert("You are not logged in. Please log in to search for tutors.");
+//        window.location.assign("../index.html");
     }
 });
