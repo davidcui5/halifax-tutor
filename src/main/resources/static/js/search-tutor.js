24a9286fa50edb228478a49dbbf6d1a4af99bcd8
Zongming Liu <zongmingxliu@gmail.com>
2018-07-22 01:36:28 -0300

    almost finish the front-end part

--- src/main/resources/static/js/search-tutor.js
+++ src/main/resources/static/js/search-tutor.js
@@ -1,36 +1,47 @@
 'use strict';
 
+function processResults(success, numOfResults, results) {
+    if (!success) {
+        alert("Oops! Something went wrong in our server. Please try again");
+    } else {
+        let text = "The number of results found is: " + numOfResults;
+        console.log(text);
+        document.getElementById("numOfResultsText").innerHTML = text;
+        $("#search-results-filters").show();
+    }
+}
+
 $(document).ready(function () {
     let token = localStorage.getItem("token");
     if (token === null) {
-        alert("You are not logged in. Please log in to search for tutors.");
+        console.log("You are not logged in. Please log in to search for tutors.");
 //        window.location.assign("../index.html");
     }
     $("form").submit(function (event) {
         event.preventDefault();
         let school = $("#university-select").val();
         let courseName = $("#course-id").val();
-        let data = {
+        let searchData = {
             "school": school,
-            "courseName": courseId
+            "courseName": courseName
         };
+
         $.ajax({
             url: location.origin + "/search",
-            data: JSON.stringify(data),
+            data: JSON.stringify(searchData),
             type: "POST",
-            dataType: "json",
-            contentType: "application/json"
-        })
-        .done(function (json) {
-            alert(json);
-        })
-        .fail(function( xhr, status, errorThrown ) {
+            contentType: "application/json",
+            dataType: "json"
+        }).done(function (data) {
+            let success = data["success"];
+            let numOfResults = data["numOfResults"];
+            let results = data["results"];
+            processResults(success, numOfResults, results);
+        }).fail(function (xhr, status, errorThrown) {
             alert( "Sorry, there was a problem!" );
             console.log( "Error: " + errorThrown );
             console.log( "Status: " + status );
             console.dir( xhr );
         });
     });
-
-
 });
