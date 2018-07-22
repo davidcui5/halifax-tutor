4f053b9ff9e7dffb05d240995ae3a8289a704be0
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-07-22 16:59:19 -0300

    Add StudentController

---
+++ src/main/java/group12/student_setting/StudentController.java
@@ -0,0 +1,9 @@
+package group12.student_setting;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/student/setting")
+public class StudentController {
+}
