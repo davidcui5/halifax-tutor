1aed9702ec9ce2fa1bee3b117d07c485394572be
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-07-23 18:57:06 -0300

    Add error handling to the Student Setting page

--- src/main/java/group12/student_setting/StudentSettingService.java
+++ src/main/java/group12/student_setting/StudentSettingService.java
@@ -2,7 +2,6 @@ package group12.student_setting;
 
 import group12.data_access.IDataAccessObject;
 import group12.email.IMailer;
-import group12.email.SpringMailer;
 import group12.encryption.IEncryptor;
 import group12.encryption.SimpleMD5Encryptor;
 import group12.token_auth.IAccessToken;
@@ -49,105 +48,131 @@ public class StudentSettingService implements IStudentSetting {
         this.mailer = mailer;
     }
 
-    private boolean isAuthorized(String token) {
-        String email = decoder.decodeToken(token);
-        int count = db.countOfUserWithEmail(email);
-        if (count == 1) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    @Override
-    public String authorizeStudent(String token) {
-        if (isAuthorized(token))
-            return AUTHORIZED;
-        else return UNAUTHORIZED;
-    }
-
-
-    @Override
-    public String changePassword(String token, String password) {
-        if (isAuthorized(token)) {
+    private boolean isAuthorized(String token) throws Exception {
+        try {
+            boolean result = false;
             String email = decoder.decodeToken(token);
-            logger.log(Level.INFO, email);
-            IEncryptor encryptor = new SimpleMD5Encryptor();
-            String encryptPassword = encryptor.encrypt(password);
-            logger.log(Level.INFO, encryptPassword);
-            if (db.updateStudentPassword(email, encryptPassword)) {
-                return SUCCESS;
-            } else {
-                return FAILURE;
+            int count = db.countOfUserWithEmail(email);
+            if (count == 1) {
+                result = true;
             }
-        } else {
-            return UNAUTHORIZED;
+            return result;
+        } catch (Exception ex) {
+            throw new Exception(ex);
+        }
+    }
+
+    @Override
+    public String authorizeStudent(String token) throws Exception {
+        try {
+            if (isAuthorized(token))
+                return AUTHORIZED;
+            else return UNAUTHORIZED;
+        } catch (Exception ex) {
+            throw new Exception(ex);
         }
     }
 
 
     @Override
-    public String changeEmail(String token, String email) {
-        if (isAuthorized(token)) {
-            String oldEmail = decoder.decodeToken(token);
-            logger.log(Level.INFO, email);
-            String newEmail = email;
-            logger.log(Level.INFO, newEmail);
-            if (db.updateStudentEmail(oldEmail, newEmail)) {
-                return SUCCESS;
+    public String changePassword(String token, String password) throws Exception {
+        try {
+            if (isAuthorized(token)) {
+                String email = decoder.decodeToken(token);
+                logger.log(Level.INFO, email);
+                IEncryptor encryptor = new SimpleMD5Encryptor();
+                String encryptPassword = encryptor.encrypt(password);
+                logger.log(Level.INFO, encryptPassword);
+                if (db.updateStudentPassword(email, encryptPassword)) {
+                    return SUCCESS;
+                } else {
+                    return FAILURE;
+                }
             } else {
-                return FAILURE;
+                return UNAUTHORIZED;
             }
-        } else {
-            return UNAUTHORIZED;
+        } catch (Exception ex) {
+            throw new Exception(ex);
         }
     }
 
-    @Override
-    public String resendActivateCode(String token) {
-        if (isAuthorized(token)) {
-            String email = decoder.decodeToken(token);
-            int studentID = db.getStudentIDByEmail(email);
-            UUID uuid = UUID.randomUUID();
-            db.saveActivationCode(uuid.toString());
-            mailer.sendMail(emailSender, email, "Activation",
-                    "Activation " + serverURL + "/student/studentid/" + studentID + "/activation/" + uuid.toString() + "/");
-            return "The activation link sent your email.";
-        } else {
-            return UNAUTHORIZED;
-        }
-    }
 
     @Override
-    public String checkActivationStatus(String token) {
-        if (isAuthorized(token)) {
-            String email = decoder.decodeToken(token);
-            logger.log(Level.INFO, email);
-            int studentActivationStatus = db.getStudentActivationStatus(email);
-            if (studentActivationStatus == 1)
-                return ACTIVE;
-            else
-                return NOT_ACTIVE;
-        } else {
-            return UNAUTHORIZED;
-        }
-    }
-
-    @Override
-    public String changePhone(String token, String phone) {
-        if (isAuthorized(token)) {
-            String email = decoder.decodeToken(token);
-            logger.log(Level.INFO, email);
-            logger.log(Level.INFO, phone);
-            if (db.updateStudentPhone(email, phone)) {
-                return SUCCESS;
+    public String changeEmail(String token, String email) throws Exception {
+        try {
+            if (isAuthorized(token)) {
+                String oldEmail = decoder.decodeToken(token);
+                logger.log(Level.INFO, email);
+                String newEmail = email;
+                logger.log(Level.INFO, newEmail);
+                if (db.updateStudentEmail(oldEmail, newEmail)) {
+                    return SUCCESS;
+                } else {
+                    return FAILURE;
+                }
             } else {
-                return FAILURE;
+                return UNAUTHORIZED;
             }
-        } else {
-            return UNAUTHORIZED;
+        } catch (Exception ex) {
+            throw new Exception(ex);
         }
     }
 
+    @Override
+    public String resendActivateCode(String token) throws Exception {
+        try {
+            if (isAuthorized(token)) {
+                String email = decoder.decodeToken(token);
+                int studentID = db.getStudentIDByEmail(email);
+                UUID uuid = UUID.randomUUID();
+                db.saveActivationCode(uuid.toString());
+                mailer.sendMail(emailSender, email, "Activation",
+                        "Activation " + serverURL + "/student/studentid/" + studentID + "/activation/" + uuid.toString() + "/");
+                return "The activation link sent your email.";
+            } else {
+                return UNAUTHORIZED;
+            }
+        } catch (Exception ex) {
+            throw new Exception(ex);
+        }
+    }
 
+    @Override
+    public String checkActivationStatus(String token) throws Exception {
+        try {
+            if (isAuthorized(token)) {
+                String email = decoder.decodeToken(token);
+                logger.log(Level.INFO, email);
+                int studentActivationStatus = db.getStudentActivationStatus(email);
+                if (studentActivationStatus == 1)
+                    return ACTIVE;
+                else
+                    return NOT_ACTIVE;
+            } else {
+                return UNAUTHORIZED;
+            }
+        } catch (Exception ex) {
+            throw new Exception(ex);
+        }
+    }
+
+    @Override
+    public String changePhone(String token, String phone) throws Exception {
+        try {
+            if (isAuthorized(token)) {
+                String email = decoder.decodeToken(token);
+                logger.log(Level.INFO, email);
+                logger.log(Level.INFO, phone);
+                if (db.updateStudentPhone(email, phone)) {
+                    return SUCCESS;
+                } else {
+                    return FAILURE;
+                }
+            } else {
+                return UNAUTHORIZED;
+            }
+        } catch (Exception ex) {
+            throw new Exception(ex);
+        }
+    }
 }
