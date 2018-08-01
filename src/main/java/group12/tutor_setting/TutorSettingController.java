ea5586895783c1cfdc72b5fcbba2c9769ab44a12
Zongming Liu <zongmingxliu@gmail.com>
2018-08-01 02:38:30 -0300

    tutor setting - controller and DAOImpl - rafactoring

--- src/main/java/group12/tutor_setting/TutorSettingController.java
+++ src/main/java/group12/tutor_setting/TutorSettingController.java
@@ -8,7 +8,6 @@ import group12.tutor_setting.request.WeeklyScheduleRequest;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/group12/tutor_setting/TutorSettingDAOImpl.java
+++ src/main/java/group12/tutor_setting/TutorSettingDAOImpl.java
@@ -6,40 +6,41 @@ import group12.data_access.tutor_setting.WeeklySchedule;
 import group12.data_access.tutor_setting.UpdateTutorWeeklyScheduleSQLOperation;
 
 public class TutorSettingDAOImpl implements ITutorSettingDAO {
+    private SQLOperationTemplate operation;
 
     @Override
     public boolean setTutorPassword(String email, String password) {
-        SQLOperationTemplate operation = new UpdateTutorPasswordSQLOperation(email, password);
+        operation = new UpdateTutorPasswordSQLOperation(email, password);
         return (Boolean) operation.executeMysqlQuery();
     }
 
     @Override
     public boolean setTutorEmail(String email, String newemail) {
-        SQLOperationTemplate operation = new UpdateTutorEmailSQLOperation(email, newemail);
+        operation = new UpdateTutorEmailSQLOperation(email, newemail);
         return (Boolean) operation.executeMysqlQuery();
     }
 
     @Override
     public boolean setTutorPhone(String email, String phone) {
-        SQLOperationTemplate opeation = new UpdateTutorPhoneSQLOperation(email, phone);
-        return (Boolean) opeation.executeMysqlQuery();
+        operation = new UpdateTutorPhoneSQLOperation(email, phone);
+        return (Boolean) operation.executeMysqlQuery();
     }
 
     @Override
     public boolean setTutorCard(String email, String creditCardHolder, String creditCardNum, String creditCardExpiryDate, int securityCode) {
-        SQLOperationTemplate operation = new UpdateTutorCardSQLOperation(email, creditCardHolder, creditCardNum, creditCardExpiryDate, securityCode);
+        operation = new UpdateTutorCardSQLOperation(email, creditCardHolder, creditCardNum, creditCardExpiryDate, securityCode);
         return (Boolean) operation.executeMysqlQuery();
     }
 
     @Override
     public boolean setEducation(String email, String education) {
-        SQLOperationTemplate operation = new UpdateTutorEducationSQLOperation(email, education);
+        operation = new UpdateTutorEducationSQLOperation(email, education);
         return (Boolean) operation.executeMysqlQuery();
     }
 
     @Override
     public boolean setExperience(String email, String experience) {
-        SQLOperationTemplate operation = new UpdateTutorExperienceSQLOperation(email, experience);
+        operation = new UpdateTutorExperienceSQLOperation(email, experience);
         return (Boolean) operation.executeMysqlQuery();
     }
 
@@ -50,16 +51,16 @@ public class TutorSettingDAOImpl implements ITutorSettingDAO {
 
     @Override
     public boolean setWeeklySchedule(String email, WeeklySchedule weeklySchedule) {
-        SQLOperationTemplate operation = new GetTutorIdSQLOperation(email);
-        Tutor tutor = (Tutor) operation.executeMysqlQuery();
-        int tutorId = tutor.getTutorID();
+        IDataAccessObject dataAccessObject = new MysqlDAOImpl();
+        int tutorId = dataAccessObject.getTutorIDByEmail(email);
+
         operation = new UpdateTutorWeeklyScheduleSQLOperation(tutorId, weeklySchedule);
         return (Boolean) ((UpdateTutorWeeklyScheduleSQLOperation) operation).executeMysqlUpdate();
     }
 
     @Override
     public boolean setPlan(String email, String planNo) {
-        SQLOperationTemplate operation = new UpdateTutorSbuscriptionSQLOperation(email, planNo);
+        operation = new UpdateTutorSbuscriptionSQLOperation(email, planNo);
         return (Boolean) operation.executeMysqlQuery();
     }
 
