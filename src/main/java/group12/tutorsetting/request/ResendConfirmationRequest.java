471c73400ef8d5b95cbab157d611d4ec95552f00
Zongming Liu <zongmingxliu@gmail.com>
2018-08-01 04:14:22 -0300

    add resend confirmation email request

---
+++ src/main/java/group12/tutorsetting/request/ResendConfirmationRequest.java
@@ -0,0 +1,13 @@
+package group12.tutorsetting.request;
+
+public class ResendConfirmationRequest {
+    private String email;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
